940b28415200901414e7b0336cfca9058a1d18cc
Harun Altay <h.altay@gmail.com>
2019-03-29 10:47:44 +0300

    norvig üzerinden geçildi

---
+++ Norvig.PyCharm/merhaba.py
@@ -0,0 +1,22 @@
+from spell import *
+
+print("Merhaba Dünya!")
+
+# output = correction("spelxying")
+
+# output = len(edits1("somthing"))
+
+# output = known(edits1("somthing"))
+
+# output = len(WORDS)
+
+# output = sum(WORDS.values())
+
+# output = WORDS.most_common(5)
+
+# output = max(WORDS, key=P)
+
+output = P('the')
+
+print(output)
+
--- Norvig.PyCharm/spell.py
+++ Norvig.PyCharm/spell.py
@@ -8,25 +8,32 @@ MIT license: www.opensource.org/licenses/mit-license.php
 import re
 from collections import Counter
 
+
 def words(text): return re.findall(r'\w+', text.lower())
+
 
 WORDS = Counter(words(open('big.txt').read()))
 
-def P(word, N=sum(WORDS.values())): 
+
+def P(word, N=sum(WORDS.values())):
     "Probability of `word`."
     return WORDS[word] / N
+
 
 def correction(word): 
     "Most probable spelling correction for word."
     return max(candidates(word), key=P)
 
+
 def candidates(word): 
     "Generate possible spelling corrections for word."
     return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
 
+
 def known(words): 
     "The subset of `words` that appear in the dictionary of WORDS."
     return set(w for w in words if w in WORDS)
+
 
 def edits1(word):
     "All edits that are one edit away from `word`."
@@ -38,11 +45,14 @@ def edits1(word):
     inserts    = [L + c + R               for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
+
 def edits2(word): 
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
-################ Test Code 
+
+#  ############### Test Code
+
 
 def unit_tests():
     assert correction('speling') == 'spelling'              # insert
@@ -75,10 +85,12 @@ def unit_tests():
     assert 0.07 < P('the') < 0.08
     return 'unit_tests pass'
 
+
 def spelltest(tests, verbose=False):
     "Run correction(wrong) on all (right, wrong) pairs; report results."
     import time
-    start = time.clock()
+    # start = time.clock()
+    start = time.perf_counter()
     good, unknown = 0, 0
     n = len(tests)
     for right, wrong in tests:
@@ -89,17 +101,27 @@ def spelltest(tests, verbose=False):
             if verbose:
                 print('correction({}) => {} ({}); expected {} ({})'
                       .format(wrong, w, WORDS[w], right, WORDS[right]))
-    dt = time.clock() - start
+    # dt = time.clock() - start
+    dt = time.perf_counter() - start
     print('{:.0%} of {} correct ({:.0%} unknown) at {:.0f} words per second '
           .format(good / n, n, unknown / n, n / dt))
-    
+
+
 def Testset(lines):
     "Parse 'right: wrong1 wrong2' lines into [('right', 'wrong1'), ('right', 'wrong2')] pairs."
     return [(right, wrong)
             for (right, wrongs) in (line.split(':') for line in lines)
             for wrong in wrongs.split()]
 
+
 if __name__ == '__main__':
     print(unit_tests())
     spelltest(Testset(open('spell-testset1.txt')))
-spelltest(Testset(open('spell-testset2.txt')))
+    # spelltest(Testset(open('spell-testset2.txt')))
+
+
+# use instead of time.clock() depreated :
+# time.perf_counter()
+# or
+# time.process_time()
+
